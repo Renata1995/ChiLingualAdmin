2be975b3c0c5f399913bce1a4b9329e9439fa6bf
Renata1995 <zchai13@austincollege.edu>
2016-03-06 20:53:39 -0600

    edit UserController

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,8 +6,11 @@ class BootStrap {
 	Random rand=new Random()
 	
     def init = { servletContext ->
-		
-		
+		User ablock=new User(username:"ablock@austincollege.edu",password:"ablock").save(flush:true)
+		User mhiggs=new User(username:"mhiggs@austincollege.edu",password:"mhiggs").save(flush:true)
+		Role admin=new Role(authority:"ROLE_ADMIN",enabled:true).save(flush:true)
+		UserRole.create(ablock,admin,true)
+		UserRole.create(mhiggs,admin,true)
 	}	
 	
     def destroy = {
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,23 @@
 // Place your Spring DSL code here
+import wslite.rest.*
 beans = {
+	clientBasicAuth(wslite.http.auth.HTTPBasicAuthorization) {
+		username = "Aladdin"
+		password = "open sesame"
+	}
+	
+	httpClient(wslite.http.HTTPClient) {
+		connectTimeout = 5000
+		readTimeout = 10000
+		useCaches = false
+		followRedirects = false
+		sslTrustAllCerts = true
+		// authorization = ref('clientBasicAuth')
+		// proxy = myproxy
+	}
+	restClient(wslite.rest.RESTClient) {
+		url = "http://localhost:8090/APITesting/"
+		httpClient = ref('httpClient')
+		authorization = ref('clientBasicAuth')
+	}
 }
